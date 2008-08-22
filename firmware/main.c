c410874ae5194766e871b94e1814b81256b431a2
Johannes Krude <johannes@krude.de>
2008-08-22 22:36:18 +0200

    added firmware

---
+++ firmware/main.c
@@ -0,0 +1,60 @@
+/*
+ * © Johannes Krude 2008
+ *
+ * This file is part of usb-Relay.
+ *
+ * usb-Relay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * usb-Relay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with usb-Relay.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "main_obdev.c"
+
+/*
+Pin assignment:
+PB0, PB2 = USB data lines
+*/
+
+#define BIT_SWITCH 3
+
+#define REQUESTTYPE_D2H         0x80
+
+#define REQUEST_DEVICE_COUNT 0
+#define REQUEST_SET_RELAYS   1
+
+/* ------------------------------------------------------------------------- */
+
+uchar	usbFunctionSetup(uchar data[8])
+{
+	usbRequest_t *request= (usbRequest_t*) data;
+	static uchar one= 1;
+	switch (request->bRequest) {
+	case REQUEST_DEVICE_COUNT:
+		if (((request->bmRequestType & REQUESTTYPE_D2H) != 0) && (request->wLength.word == 1)) {
+			usbMsgPtr= &one;
+			return 1; // we want to send the requested data
+		}
+		break;
+	case REQUEST_SET_RELAYS:
+		if (request->wValue.bytes[0] & 0x01)
+			PORTB|= (1<<BIT_SWITCH);
+		else
+			PORTB&= ~(1<<BIT_SWITCH);
+	}
+	return 0;
+}
+
+uchar main(void)
+{
+	DDRB= (1<<3);
+	main_obdev();
+}
---
+++ firmware/main_obdev.c
@@ -0,0 +1,109 @@
+/* Name: main.c
+ * Project: Thermostat based on AVR USB driver
+ * Author: Christian Starkjohann
+ * Creation Date: 2006-04-23
+ * Tabsize: 4
+ * Copyright: (c) 2006 by OBJECTIVE DEVELOPMENT Software GmbH
+ * License: Proprietary, free under certain conditions. See Documentation.
+ * This Revision: $Id: main.c 537 2008-02-28 21:13:01Z cs $
+ *
+ * modified by Johannes Krude
+ */
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+#include "usbdrv.h"
+
+#define UTIL_BIN4(x)        (uchar)((0##x & 01000)/64 + (0##x & 0100)/16 + (0##x & 010)/4 + (0##x & 1))
+#define UTIL_BIN8(hi, lo)   (uchar)(UTIL_BIN4(hi) * 16 + UTIL_BIN4(lo))
+
+/* ------------------------------------------------------------------------- */
+/* ------------------------ Oscillator Calibration ------------------------- */
+/* ------------------------------------------------------------------------- */
+
+/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
+ * derived from the 66 MHz peripheral clock by dividing. Our timing reference
+ * is the Start Of Frame signal (a single SE0 bit) available immediately after
+ * a USB RESET. We first do a binary search for the OSCCAL value and then
+ * optimize this value with a neighboorhod search.
+ * This algorithm may also be used to calibrate the RC oscillator directly to
+ * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
+ * is wide outside the spec for the OSCCAL value and the required precision for
+ * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
+ * experimental purposes only!
+ */
+static void calibrateOscillator(void)
+{
+uchar       step = 128;
+uchar       trialValue = 0, optimumValue;
+int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+
+    /* do a binary search: */
+    do{
+        OSCCAL = trialValue + step;
+        x = usbMeasureFrameLength();    /* proportional to current real frequency */
+        if(x < targetValue)             /* frequency still too low */
+            trialValue += step;
+        step >>= 1;
+    }while(step > 0);
+    /* We have a precision of +/- 1 for optimum OSCCAL here */
+    /* now do a neighborhood search for optimum value */
+    optimumValue = trialValue;
+    optimumDev = x; /* this is certainly far away from optimum */
+    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
+        x = usbMeasureFrameLength() - targetValue;
+        if(x < 0)
+            x = -x;
+        if(x < optimumDev){
+            optimumDev = x;
+            optimumValue = OSCCAL;
+        }
+    }
+    OSCCAL = optimumValue;
+}
+/*
+Note: This calibration algorithm may try OSCCAL values of up to 192 even if
+the optimum value is far below 192. It may therefore exceed the allowed clock
+frequency of the CPU in low voltage designs!
+You may replace this search algorithm with any other algorithm you like if
+you have additional constraints such as a maximum CPU clock.
+For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
+ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
+both regions.
+*/
+
+void    usbEventResetReady(void)
+{
+    calibrateOscillator();
+    eeprom_write_byte(0, OSCCAL);   /* store the calibrated value in EEPROM */
+}
+
+/* ------------------------------------------------------------------------- */
+/* --------------------------------- main ---------------------------------- */
+/* ------------------------------------------------------------------------- */
+
+int main_obdev(void)
+{
+uchar   i;
+uchar   calibrationValue;
+    calibrationValue = eeprom_read_byte(0); /* calibration value from last time */
+    if(calibrationValue != 0xff){
+        OSCCAL = calibrationValue;
+    }
+    usbDeviceDisconnect();
+    for(i=0;i<20;i++){  /* 300 ms disconnect */
+        _delay_ms(15);
+    }
+    usbDeviceConnect();
+    usbInit();
+    sei();
+    for(;;){    /* main event loop */
+        usbPoll();
+    }
+    return 0;
+}
